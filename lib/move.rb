43d1e9ac6fb6ee2f3eccb34c698a887cb2de6b3c
Janis Lebron <janislebron@gmail.com>
2020-04-26 18:26:35 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,9 +7,9 @@ def display_board(board)
 end
 
 def input_to_index(input)
-  puts input.to_i
+  input.to_i
   index = input.to_i
-  puts index - 1
+  return index - 1
 end
 
 def move(array, index, character = "X")
